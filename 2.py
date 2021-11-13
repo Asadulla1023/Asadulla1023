6d54a0ed235ac818e273fc9a8d089f16f158bde3
Sayfullayev Asadulla <sayfullayevasadulla1@gmail.com>
2021-11-13 15:59:50 +0500

    imported random and floor functions

---
+++ 2.py
@@ -0,0 +1,16 @@
+asadulla@asadulla:~/Desktop/Darslar$ git add .
+asadulla@asadulla:~/Desktop/Darslar$ git commit -m "Added 1.py file"
+[master 8a50b40] Added 1.py file
+ 1 file changed, 2 insertions(+)
+asadulla@asadulla:~/Desktop/Darslar$ git push -u origin master 
+Enumerating objects: 5, done.
+Counting objects: 100% (5/5), done.
+Writing objects: 100% (3/3), 290 bytes | 290.00 KiB/s, done.
+Total 3 (delta 0), reused 0 (delta 0)
+To https://github.com/Asadulla1023/Asadulla1023
+   d80390e..8a50b40  master -> master
+Branch 'master' set up to track remote branch 'master' from 'origin'.
+asadulla@asadulla:~/Desktop/Darslar$ 
+
+
+
---
+++ 3.py
@@ -0,0 +1,4 @@
+from random import random
+from math import floor
+print(floor(random()*100))
+
