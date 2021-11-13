598fc751ad6eb9aa2c10df3277408ccda594ef10
Sayfullayev Asadulla <sayfullayevasadulla1@gmail.com>
2021-11-13 16:13:35 +0500

    Added 3.py file

--- 3.py
+++ 3.py
@@ -2,3 +2,11 @@ from random import random
 from math import floor
 print(floor(random()*100))
 
+# names = ['Asadulla', 'Xusan', 'Xasan', 'Elbek','Asadulla']
+# print(names[len(names)-1])
+
+# names = ['Asadulla', 'Xusan', 'Xasan', 'Elbek','AsadullA']
+# print(names[2:0:-1])
+
+# names = ['Asadulla', 'Xusan', 'Xasan', 'Elbek','AsadullA']
+# print(names[2:0:-1])
